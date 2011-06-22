18bbb4dd6401ef3de7c29714b920c0c8fa95bd17
Alan Smith <alan@cloakedcode.com>
2011-06-22 09:13:36 -0400

    Debugging Pagoda, still.

--- css/less.php
+++ css/less.php
@@ -3,11 +3,9 @@
 if (empty($_GET['f']) === false)
 {
     $root = $_SERVER['DOCUMENT_ROOT'];
-    echo $root;
-    echo dirname(__FILE__);
-    require($root.'/css/lessphp/lessc.inc.php');
-    $input = $root.'/css/'.$_GET['f'].'.less';
-    $output = $root.'/cache/'.$_GET['f'].'.css';
+    require($root.'css/lessphp/lessc.inc.php');
+    $input = $root.'css/'.$_GET['f'].'.less';
+    $output = $root.'cache/'.$_GET['f'].'.css';
 
     try {
         lessc::ccompile($input, $output);
