4b4a60ac1f2cae24348db534ffb15b42422760d2
Alan Smith <alan@cloakedcode.com>
2011-06-28 20:09:08 -0400

    Edited about.html file.

--- css/less.php
+++ css/less.php
@@ -2,7 +2,7 @@
 
 if (empty($_GET['f']) === false)
 {
-    $root = $_SERVER['DOCUMENT_ROOT'];
+    $root = $_SERVER['DOCUMENT_ROOT'].'/';
     require($root.'css/lessphp/lessc.inc.php');
     $input = $root.'css/'.$_GET['f'].'.less';
     $output = $root.'cache/'.$_GET['f'].'.css';
